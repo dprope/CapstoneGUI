50785022db019c634768bf3e4011b61ff268dd04
dprope <properdc20@mail.vmi.edu>
2020-04-30 23:16:19 -0400

    change

--- app/src/main/java/com/example/mortargui/Sample_Activity.java
+++ app/src/main/java/com/example/mortargui/Sample_Activity.java
@@ -1,6 +1,12 @@
 package com.example.mortargui;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -10,6 +16,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import eu.amirs.JSON;
 import okhttp3.Call;
@@ -23,8 +32,10 @@ public class Sample_Activity extends AppCompatActivity {
     TextView textview;
 
 
-    String url = "https://vmihci.herokuapp.com/getimage";
 
+    //String url = "https://vmihci.herokuapp.com/getimage";
+
+    String url ="http://144.75.191.68:5005/getimage";
 
 
 
@@ -32,6 +43,28 @@ public class Sample_Activity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sample);
+        final Button nothreat = findViewById(R.id.nothreatbutt);
+        final Intent intent = new Intent(this, Sample_Activity.class);
+        nothreat.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                finish();
+                startActivity(intent);
+
+
+            }
+        });
+        final Button threat = findViewById(R.id.threatbutton);
+        final Intent intent2 = new Intent(this, PhoneActivity.class);
+        threat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent2);
+            }
+        });
+
+
+
+
         imageView = (ImageView) findViewById(R.id.imageee);
         loadImageFromUrl(url);
         textview = (TextView) findViewById(R.id.texx);
@@ -71,9 +104,33 @@ public class Sample_Activity extends AppCompatActivity {
         });
 
 
+
+
+
     }
 
+    private void loadImagefromURL_HTTP(String url){
 
+        try{
+            //String url = "http://172.22.30.55:8080/data/product_images/"+data.get("col_1");
+            URL ulrn = new URL(url);
+            HttpURLConnection con = (HttpURLConnection)ulrn.openConnection();
+            InputStream is = con.getInputStream();
+            Bitmap bmp = BitmapFactory.decodeStream(is);
+            if (null != bmp)
+                imageView.setImageBitmap(bmp);
+            else
+                System.out.println("Bitmap is NULL");
+
+            if (con != null) {
+                con.disconnect();
+
+            }
+        } catch(Exception e) {
+            //e.printStackTrace();
+            Log.d("debug", e.getMessage());
+        }
+    }
 
     private void loadImageFromUrl(String url) {
         Picasso.with(this).load(url).placeholder(R.mipmap.ic_launcher)
@@ -93,5 +150,8 @@ public class Sample_Activity extends AppCompatActivity {
                 });
 
 
+
     }
+
+
 }
