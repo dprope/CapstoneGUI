580802fb5521740af40fa2577d138dc377b05847
dprope <63425524+dprope@users.noreply.github.com>
2020-05-02 11:10:39 -0400

    Update Sample_Activity.java

--- app/src/main/java/com/example/mortargui/Sample_Activity.java
+++ app/src/main/java/com/example/mortargui/Sample_Activity.java
@@ -36,13 +36,51 @@ public class Sample_Activity extends AppCompatActivity {
     //String url = "https://vmihci.herokuapp.com/getimage";
 
     String url ="http://144.75.191.68:5005/getimage";
+    
+    Button b1;
+    private final static int SEND_SMS_PERMISSION_REQ=1;
 
-
-
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sample);
+        
+        b1 = findViewById(R.id.threatbutton);
+        b1.setEnabled(false);
+        
+        if (checkPermission(Manifest.permission.SEND_SMS)) {
+            b1.setEnabled(true);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, SEND_SMS_PERMISSION_REQ);
+
+        }
+        b1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String s2 = "POSSIBLE THREAT IN AREA";
+                String s1 = "7706246972";
+                String s3 = "3362072493";
+                if (!TextUtils.isEmpty(s1) && !TextUtils.isEmpty(s2)) {
+                    if (checkPermission(Manifest.permission.SEND_SMS)) {
+                        SmsManager smsManager = SmsManager.getDefault();
+                        smsManager.sendTextMessage(s1, null, s2, null, null);
+                        smsManager.sendTextMessage(s3, null, s2, null, null);
+
+                    } else {
+                        Toast.makeText(Sample_Activity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(Sample_Activity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+
+        });
+
+        
+      
         final Button nothreat = findViewById(R.id.nothreatbutt);
         final Intent intent = new Intent(this, Sample_Activity.class);
         nothreat.setOnClickListener(new View.OnClickListener(){
@@ -107,6 +145,21 @@ public class Sample_Activity extends AppCompatActivity {
 
 
 
+    }
+    
+       private boolean checkPermission(String sendSms) {
+        int check= ContextCompat.checkSelfPermission(this,sendSms);
+        return check== PackageManager.PERMISSION_GRANTED;
+    }
+    
+     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,@NonNull int[] grantResults){
+        switch(requestCode){
+            case SEND_SMS_PERMISSION_REQ:
+                if(grantResults.length>0 &&(grantResults[0]==PackageManager.PERMISSION_GRANTED)){
+                    b1.setEnabled(true);
+                }
+                break;
+        }
     }
 
     private void loadImagefromURL_HTTP(String url){
